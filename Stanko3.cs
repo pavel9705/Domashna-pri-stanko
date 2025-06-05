c6f4885eba798caf31e247d94980bb98cbcb8a50
pavel9705 <120485497+pavel9705@users.noreply.github.com>
2025-06-05 08:25:09 +0300

    Stanko3.cs

--- Stanko3.cs
+++ Stanko3.cs
@@ -1,4 +1,3 @@
-﻿// hint: Изчислява детерминантата на 3x3 матрица
 using System;
 using System.Linq;
 
@@ -6,15 +5,21 @@ class Stanko3
 {
     static void Main()
     {
-        Console.WriteLine("Enter 9 numbers for a 3x3 matrix, separated by ',': ");
-        var input = Console.ReadLine().Split(',').Select(double.Parse).ToArray();
-        double[,] matrix = new double[3, 3];
-        for (int i = 0; i < 9; i++) matrix[i / 3, i % 3] = input[i];
+        Console.WriteLine("Enter 9 numbers for 3x3 matrix separated by space:");
+        double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        double[,] m = new double[3, 3];
 
-        double det = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1])
-                   - matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0])
-                   + matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+        for (int i = 0; i < 9; i++)
+            m[i / 3, i % 3] = input[i];
 
-        Console.WriteLine($"Determinant: {det}");
+        Console.WriteLine("Matrix as list:");
+        Console.WriteLine(string.Join("'", input));
+
+        double det =
+            m[0, 0] * (m[1, 1] * m[2, 2] - m[1, 2] * m[2, 1]) -
+            m[0, 1] * (m[1, 0] * m[2, 2] - m[1, 2] * m[2, 0]) +
+            m[0, 2] * (m[1, 0] * m[2, 1] - m[1, 1] * m[2, 0]);
+
+        Console.WriteLine("Determinant: " + det);
     }
 }
