390ffbe648ab17fab74448a0be21890fa5e3d9f3
pavel9705 <120485497+pavel9705@users.noreply.github.com>
2025-06-05 08:28:07 +0300

    Stanko6.cs

--- Stanko6.cs
+++ Stanko6.cs
@@ -1,19 +1,25 @@
-﻿// hint: Изчислява дължините на 3D вектори
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 class Stanko6
 {
     static void Main()
     {
-        Console.Write("Enter vectors like x y z | x y z ... : ");
-        var input = Console.ReadLine().Split('|');
+        Console.WriteLine("Enter first set (numbers separated by space):");
+        var set1 = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
 
-        foreach (var vectorStr in input)
-        {
-            var coords = vectorStr.Trim().Split(' ').Select(double.Parse).ToArray();
-            double length = Math.Sqrt(coords[0] * coords[0] + coords[1] * coords[1] + coords[2] * coords[2]);
-            Console.WriteLine($"Length: {length}");
-        }
+        Console.WriteLine("Enter second set:");
+        var set2 = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+
+        var union = set1.Union(set2);
+        var intersection = set1.Intersect(set2);
+        var difference = set1.Except(set2);
+        var symmetricDiff = union.Except(intersection);
+
+        Console.WriteLine("Union: " + string.Join(" ", union));
+        Console.WriteLine("Intersection: " + string.Join(" ", intersection));
+        Console.WriteLine("Difference (Set1 - Set2): " + string.Join(" ", difference));
+        Console.WriteLine("Symmetric Difference: " + string.Join(" ", symmetricDiff));
     }
 }
