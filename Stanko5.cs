693c9350ef705c9af2d86bca79f5a457287648f8
pavel9705 <120485497+pavel9705@users.noreply.github.com>
2025-06-05 08:27:40 +0300

    Stanko5.cs

--- Stanko5.cs
+++ Stanko5.cs
@@ -1,4 +1,3 @@
-﻿// hint: Намира дължината на най-късия низ в списък
 using System;
 using System.Linq;
 
@@ -6,10 +5,21 @@ class Stanko5
 {
     static void Main()
     {
-        Console.Write("Enter strings separated by ',': ");
-        var strings = Console.ReadLine().Split(',');
-        int minLen = strings.Min(s => s.Length);
+        Console.WriteLine("Enter number of 3D vectors:");
+        int n = int.Parse(Console.ReadLine());
 
-        Console.WriteLine($"Shortest length: {minLen}");
+        double minLength = double.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Enter vector {i + 1} (x y z):");
+            double[] v = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            double length = Math.Sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+
+            if (length < minLength)
+                minLength = length;
+        }
+
+        Console.WriteLine($"Shortest vector length: {minLength:F2}");
     }
 }
